319b5f08d6d408aa8f1c2141a0b90bd63e4c72a5
Sophia Pawlisch <300040900@fvtc.edu>
2021-08-18 14:34:40 -0500

    created register in userServiceImpl

--- src/main/java/com/revature/services/UserServiceImpl.java
+++ src/main/java/com/revature/services/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.revature.services;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.revature.beans.User;
+import com.revature.beans.UserType;
 import com.revature.data.ReactiveUserDao;
+import com.revature.dto.UserDTO;
 
 public class UserServiceImpl implements UserService {
 	
@@ -19,8 +21,23 @@ public class UserServiceImpl implements UserService {
 		return null;
 	}
 	
-	public User register () {
-		return null;
+	public User register (String username, UserType userType, String firstName, String lastName, 
+			String email, String address, String storeName) {
+		User user = new User();
+		user.setUsername(username);
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setEmail(email);
+		user.setAddress(address);
+		user.setUserType(userType);
+		user.setCurrency(0d);
+		if (UserType.SELLER.equals(userType)) {
+			user.setStoreName(storeName);
+		} else {
+			user.setStoreName(null);
+		}
+		userDao.save(new UserDTO(user));
+		return user;
 	}
 
 }
