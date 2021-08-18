9cfddeaa1f23991831e3ab429f520ae9d1584e90
Sophia Pawlisch <300040900@fvtc.edu>
2021-08-18 17:13:23 -0500

    yup

--- src/main/java/com/revature/services/ItemServiceImpl.java
+++ src/main/java/com/revature/services/ItemServiceImpl.java
@@ -1,6 +1,8 @@
 package com.revature.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 import com.revature.beans.Item;
@@ -8,6 +10,7 @@ import com.revature.data.ReactiveItemDao;
 
 import reactor.core.publisher.Flux;
 
+@Service
 public class ItemServiceImpl implements ItemService {
 	private ReactiveItemDao itemDao;
 	
--- src/main/java/com/revature/services/UserServiceImpl.java
+++ src/main/java/com/revature/services/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.revature.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.revature.beans.Item;
 import com.revature.beans.User;
@@ -15,6 +16,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
 
+@Service
 public class UserServiceImpl implements UserService {
 	
 	private ReactiveUserDao userDao;
