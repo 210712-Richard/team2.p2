c2deda64546d12cf347af74bd51c00d5fa6de9ab
qhardwick <qhardwick@gmail.com>
2021-08-19 16:43:42 -0400

    login controller

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,12 +1,19 @@
 package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.WebSession;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.revature.beans.User;
 import com.revature.services.UserService;
+import com.sun.tools.sjavac.Log;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 public class UserController {
@@ -26,12 +33,20 @@ public class UserController {
 	LOGOUT(){
 		
 	}
-	
-	@PostMapping
-	LOGIN(){
-		
-	}
 	*/
+	@PostMapping  // ("/users")
+	public ResponseEntity<Mono<User>> login(@RequestBody User u, WebSession session){
+		
+		Mono<User> loggedUser = userService.login(u.getUsername());
+		
+		if(loggedUser == null) {
+			return ResponseEntity.notFound().build();		
+			}
+		
+		session.getAttributes().put("loggedUser", u);
+		return ResponseEntity.ok(loggedUser);
+	}
+	
 	
 	
 	
