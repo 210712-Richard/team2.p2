62ce3821c85b6a2cd6ce7fae6e9743fd9aac11ff
Sophia Pawlisch <300040900@fvtc.edu>
2021-08-26 19:59:06 -0500

    updating

--- src/main/java/com/revature/data/ReactiveUserDao.java
+++ src/main/java/com/revature/data/ReactiveUserDao.java
@@ -1,8 +1,13 @@
 package com.revature.data;
 
+import java.util.List;
+
+import org.springframework.data.cassandra.repository.Query;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 import org.springframework.stereotype.Repository;
 
+import com.revature.beans.Item;
+import com.revature.beans.User;
 import com.revature.dto.UserDTO;
 
 import reactor.core.publisher.Mono;
@@ -10,5 +15,5 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface ReactiveUserDao extends ReactiveCassandraRepository<UserDTO, String>{
 	Mono<UserDTO> findByUsername(String username);
-
+	
 }
---
+++ src/test/java/addToWishlist/addToWishlist.feature
@@ -0,0 +1,12 @@
+Feature: add an item to my wishlist
+Scenario: login, then the user adds an item to their wishlist
+
+Background:
+	* def signin = call read('classpath:login/login.feature')
+
+Given url 'http://localhost:8080/users/DemoUser/wishlist'
+And request { uuid : '3435aef4-fbe2-4a7e-ae5c-61f066e5ff92'}
+And cookie SESSION = signin.sessionCookie
+When method post
+Then status 200
+And match response contains { wishlist : '#null' }
