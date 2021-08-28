935415584d021d164c0c99fe0325f71652512093
Sophia Pawlisch <300040900@fvtc.edu>
2021-08-28 18:06:53 -0500

    order doesnt work but we tried

--- src/main/java/com/revature/services/OrderServiceImpl.java
+++ src/main/java/com/revature/services/OrderServiceImpl.java
@@ -1,13 +1,12 @@
 package com.revature.services;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.revature.beans.Item;
 import com.revature.beans.Order;
 import com.revature.beans.ShippingStatus;
 import com.revature.data.ReactiveItemDao;
@@ -55,23 +54,44 @@ public class OrderServiceImpl implements OrderService {
 		Order createOrder = new Order();
 		createOrder.setOrderDate(LocalDate.now());
 		createOrder.setStatus(ShippingStatus.SENT);
+		//UUID id = UUID.randomUUID();
 		createOrder.setUuid(UUID.randomUUID());
 		createOrder.setCustomer(customer);
-		OrderDTO createDTO = new OrderDTO(createOrder);
+		
+		// can't get the items list set
+		createOrder.setItems(new ArrayList<>());
 		
 		
+		return orderDao.save(new OrderDTO(createOrder)).map(or -> or.getOrder());
+		
+		// 
+		/*
 		// get shopping cart and set as orderList
 		Mono<List<Item>> orderList = Flux.from(userDao.findByUsername(customer))
-				.map(userDto -> userDto.getShoppingCart()).flatMap(listUuids -> Flux.fromIterable(listUuids))
-				.flatMap(uuid -> itemDao.findByUuid(uuid)).map(itemDto -> itemDto.getItem()).collectList();
+				.map(userDto -> userDto.getShoppingCart())
+				.flatMap(listUuids -> Flux.fromIterable(listUuids))
+				.flatMap(uuid -> itemDao.findByUuid(uuid))
+				.map(itemDto -> itemDto.getItem()).collectList();
+		
+		// set customer shopping cart to empty
+		Mono<User> emptyUser =  userDao.findByUsername(customer).flatMap(user -> {
+			List<UUID> cart = new ArrayList<>();
+			user.setShoppingCart(cart);
+			return userDao.save(user);
+		}).map(user -> user.getUser());
+		
+		return orderDao.save(createDTO).map(or -> or.getOrder());
 
+*/
+		
 		// return order and save it
-		return orderList.zipWith(orderDao.save(createDTO).map(ord -> ord.getOrder()))
-				.map(tup -> {
-					Order o = tup.getT2();
-					o.setItems(tup.getT1());
-					return o;
-				});
+//		return orderList.zipWith(orderDao.save(createDTO).map(ordr -> ordr.getOrder()))
+//				.map(tup -> {
+//					Order o = tup.getT2();
+//					o.setItems(tup.getT1());
+//					return orderDao.save(new OrderDTO(o));
+//		});
+		
 		
 	}
 	
