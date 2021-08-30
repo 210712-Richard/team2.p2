c155fbdfebb19a0cbf87e10bf66336ab56823be8
Sophia Pawlisch <300040900@fvtc.edu>
2021-08-30 13:05:31 -0500

    trying again with the tests

--- src/test/java/com/revature/services/StoreServiceTest.java
+++ src/test/java/com/revature/services/StoreServiceTest.java
@@ -1,5 +1,50 @@
 package com.revature.services;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.revature.beans.Store;
+import com.revature.data.ReactiveItemDao;
+import com.revature.data.ReactiveStoreDao;
+
 public class StoreServiceTest {
+	private static ReactiveStoreDao storeDao;
+	private static ReactiveItemDao itemDao;
+	private static StoreServiceImpl service;
+	private static Store store;
+	
+	@BeforeAll
+	public static void setUpClass() {
+		store = new Store();
+	}
+	
+	@BeforeEach
+	public void setUpTests() {
+		storeDao = Mockito.mock(ReactiveStoreDao.class);
+		itemDao = Mockito.mock(ReactiveItemDao.class);
+		service = new StoreServiceImpl(itemDao, storeDao);
+	}
+	
+	@Test
+	public void testListItems() {
+		
+	}
+	
+	@Test
+	public void testRegister() {
+		
+	}
+	
+	@Test
+	public void testLogin() {
+		
+	}
+	
+	@Test
+	public void testAddItemToInventory() {
+		
+	}
 
 }
